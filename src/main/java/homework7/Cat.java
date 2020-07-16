f02c70a25634bbd9123d2aa50bb857f8f5a20fde
MaximPnk <adm.mjkee@yandex.ru>
2020-07-16 11:22:12 +0400

    finish hw7

---
+++ src/main/java/homework7/Cat.java
@@ -0,0 +1,31 @@
+package homework7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean notHungry;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate plate) {
+        if (appetite <= plate.getFood()) {
+            plate.decrease(appetite);
+            this.appetite = 0;
+            notHungry = true;
+        } else {
+            System.out.println(this.name + " не может покушать. Корма не хватает.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", appetite=" + appetite +
+                ", notHungry=" + notHungry +
+                '}';
+    }
+}
---
+++ src/main/java/homework7/Plate.java
@@ -0,0 +1,21 @@
+package homework7;
+
+public class Plate {
+    private int food;
+
+    public void info() {
+        System.out.println("В миске " + food + " еды.");
+    }
+
+    public void decrease(int appetite) {
+        food -= appetite;
+    }
+
+    public void increase(int food) {
+        this.food += food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+}
