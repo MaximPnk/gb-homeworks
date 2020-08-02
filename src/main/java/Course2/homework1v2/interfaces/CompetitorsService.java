f09322526cb6da05ee2cfcd6870a7e9207a2f6b5
MaximPnk <adm.mjkee@yandex.ru>
2020-08-02 14:02:22 +0300

    finish hw2 course2

---
+++ src/main/java/Course2/homework1v2/interfaces/CompetitorsService.java
@@ -0,0 +1,18 @@
+package Course2.homework1v2.interfaces;
+
+import Course2.homework1v2.models.*;
+
+public interface CompetitorsService {
+
+    void jump(Human human, Wall wall);
+
+    void run(Human human, Track track);
+
+    void jump(Robot robot, Wall wall);
+
+    void run(Robot robot, Track track);
+
+    void jump(Cat cat, Wall wall);
+
+    void run(Cat cat, Track track);
+}
---
+++ src/main/java/Course2/homework2/Main.java
@@ -0,0 +1,71 @@
+package Course2.homework2;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws MyArrayDataException {
+
+        String[][] strArr = new String[4][4];
+        int num = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            for (int j = 0; j < strArr[i].length; j++) {
+                strArr[i][j] = String.valueOf(num++);
+            }
+        }
+//        strArr[1][3] = "asd";
+//        strArr[0][0] = "s";
+
+        try {
+            System.out.println("Сумма всех ячеек массива = " + sumOfStrArray(strArr));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int sumOfStrArray(String[][] arr) {
+        int sum = 0;
+
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+
+        /*
+         * Можно забегая вперед сделать таким способом вместо try-catch
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                Pattern p = Pattern.compile("\\D");
+                Matcher m = p.matcher(arr[i][j]);
+                if (m.find()) {
+                    throw new MyArrayDataException(i, j);
+                }
+                sum += Integer.parseInt(arr[i][j]);
+            }
+        }
+         *
+         */
+
+        return sum;
+    }
+}
